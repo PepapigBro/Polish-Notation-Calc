075c21bb88a09a1af42deb1435950c66db23e43f
Dmitry Grachev <miktor_g@mail.ru>
2017-10-30 14:44:26 +0300

    add library

---
+++ ConsoleApp/Program.cs
@@ -0,0 +1,42 @@
+﻿using PolishCalcLibrary;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                Console.WriteLine("Введите выражение (e - exit)");
+                string userInput = Console.ReadLine();
+
+                //Пользовательсикй ввод, "- * / 15 - 7 + 1 1 3 + 2 + 1 1"
+                //  -*/15- 7 2 3 + 2 2
+                //  -*/15 5 3 4
+                //   -* 3 3 4
+                //   -9 4
+                //   5
+                if (userInput == "e") { break; }
+
+                PolishCalc polishcalc = new PolishCalc(userInput);
+                double? result = polishcalc.Calculate();
+
+                if (result !=null)
+                {
+                    Console.WriteLine("Result: {0}", result);
+                    Console.WriteLine();
+                }
+                else
+                {
+                    Console.WriteLine("Error");
+                }
+            }
+        }
+    }
+}
---
+++ PolishCalc/PolishCalc.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace PolishCalcLibrary
+{
+   
+    public class PolishCalc
+    {
+
+        private string userInput;
+        private  double? ExecuteAnOperation(string[] opearands, char _operator)
+        {
+            switch (_operator)
+            {
+                case '+':
+                    return Convert.ToDouble(opearands[0]) + Convert.ToDouble(opearands[1]);
+                case '-':
+                    return Convert.ToDouble(opearands[0]) - Convert.ToDouble(opearands[1]);
+                case '*':
+                    return Convert.ToDouble(opearands[0]) * Convert.ToDouble(opearands[1]);
+                case '/':
+                    return Convert.ToDouble(opearands[0]) / Convert.ToDouble(opearands[1]);
+            }
+            return null;
+
+        }
+
+        public PolishCalc(string userInput) {
+            this.userInput = userInput;
+        }
+
+        public double? Calculate() {
+            double? result = null;
+            bool errorEnabled = false;
+            while (true)
+            {
+                //Получение всех выражений вида "+ 1 2" - оператор и два операнда справа
+                Regex regex = new Regex(@"[-*+/]\s*\d+(\,\d+)?\s+\d+(\,\d+)?");
+                MatchCollection matches = regex.Matches(userInput);
+
+                if (matches.Count == 0) { break; }
+
+                foreach (Match matchItem in matches)
+                {
+                    string itemExpr = matchItem.Value;
+                    //замена всех мультипробелов на одинарные пробелы
+                    itemExpr = Regex.Replace(itemExpr, @"\s+", " ");
+
+                    //извлечение операндов
+                    string[] operands = itemExpr.Split(new char[] { '*', '/', '+', '-', ' ' }, StringSplitOptions.RemoveEmptyEntries);
+
+                    //извлечение оператора
+                    char _operator = itemExpr[0];
+
+                    //получение результата мат. операции, null - признак ошибки
+                    double? resultIteration = ExecuteAnOperation(operands, _operator);
+
+                    if (resultIteration != null)
+                    {
+                        userInput = userInput.Replace(matchItem.Value, " " + resultIteration.ToString());
+                    }
+                    else { errorEnabled = true;  break; }
+                }
+
+
+
+            }
+            if (errorEnabled == false) { result = Convert.ToDouble(userInput); }
+
+            return result;
+
+
+        }
+
+
+    }
+}
+
+
+
+
